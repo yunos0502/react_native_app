781b13495082150863c354a930cd959a1dd84dae
Park Gyu Ae <yunos0502@gmail.com>
2021-01-19 00:15:53 +0900

    add ActivityIndicator

--- src/picker.js
+++ src/picker.js
@@ -9,7 +9,7 @@
  */
 
 import React, {Component} from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, Text, ActivityIndicator} from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import Slider from '@react-native-community/slider';
 
@@ -43,6 +43,12 @@ class PickerComponent extends Component {
           step={1}
         />
         <Text style={styles.input}>{this.state.value}</Text>
+        <ActivityIndicator
+          style={{paddingTop: 200}}
+          size="large"
+          color="green"
+          animating="true"
+        />
         <Picker
           style={{height: 50, width: 250}}
           selectedValue={this.state.country}
